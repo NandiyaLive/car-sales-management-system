56d1349cc93e80310ecb3e02814901abc18d1b2e
unknown <neranjanaprsd@gmail.com>
2023-12-27 21:20:03 +0530

    initial commit

---
+++ car-sale-management/Home.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_sale_management
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            
+
+            Login loginForm = new Login();
+
+
+            this.Hide();
+            loginForm.Show();
+
+        }
+    }
+}
